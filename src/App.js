265f939de8d9144fa31f251cefca27c864b543c2
tauseef.n89 <tauseef.n89@gmail.com>
2021-06-24 22:13:16 +0530

    all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Products from './admin/Products'
+import Main from './main/Main'
+import { BrowserRouter, Route } from 'react-router-dom'
+import ProductCreate from './admin/ProductCreate'
+import ProductEdit from './admin/ProductEdit'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Route path='/' exact component={Main} />
+        <Route path='/admin/products' exact component={Products} />
+        <Route path='/admin/products/create' exact component={ProductCreate} />
+        <Route path='/admin/products/:id/edit' exact component={ProductEdit} />
+      </BrowserRouter>      
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/admin/ProductCreate.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
+import Wrapper from './Wrapper'
+
+function ProductCreate() {
+    const [title, setTitle] = useState('');
+    const [image, setImage] = useState('');
+
+    const history = useHistory();
+
+    const submit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:3004/products', {
+            method: 'POST',
+            headers: {"Content-type": "application/json"},
+            body: JSON.stringify({ title, image })
+        }).then(() => history.push('/admin/products'));
+    }
+
+    return (
+        <Wrapper>
+            <form onSubmit={submit}>
+                <label>Title</label>
+                <input type="text" name="title" 
+                onChange={e => setTitle(e.target.value)}
+                 />
+                <label>Image</label>
+                <input type="text" name="image"
+                onChange={e => setImage(e.target.value)}
+                
+                />
+                <button type="submit">Add Product</button>
+            </form>
+        </Wrapper>
+    )
+}
+
+export default ProductCreate
